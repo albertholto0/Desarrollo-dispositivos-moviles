f0829bf16b9c8f51f4061597160a8c0795adeaff
Albert Contreras <albertmendoza019@gmail.com>
2025-04-02 12:37:44 -0600

    Segundo commit

--- primer_app/lib/main.dart
+++ primer_app/lib/main.dart
@@ -30,6 +30,30 @@ void main() {
     ),
   );
   class MyWidget extends StatelessWidget {
-    
+    Widget build(BuildContext context) {
+      return Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            radius: 1,
+            colors: [
+              Color.fromARGB(255, 255, 255, 255),
+              Color.fromARGB(255, 255, 212, 84),
+            ],
+          ),
+        ),
+        child: const Center(
+          child: Text(
+            "2 de agua por \n una de arroz",
+            style: TextStyle(
+              color: Color.fromARGB(255, 61, 49, 4),
+              fontSize: 30,
+              fontFamily: "Times New Roman",
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      );
+    }
+    return
   }
 }
